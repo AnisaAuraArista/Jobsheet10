e7b082df1061e1316e1fc1faa9badc8611bb7282
PraktikumDaspro <email@ristapolinema03.com>
2025-11-13 01:00:00 +0700

    modifikasi null

--- BioskopWithScanner04.java
+++ BioskopWithScanner04.java
@@ -20,20 +20,25 @@ public class BioskopWithScanner04 {
                     while (true) {
                         System.out.print("Masukkan nama: ");
                         String nama = sc.nextLine();
-                        System.out.print("Masukkan baris (1-4): ");
-                        int baris = sc.nextInt();
-                        System.out.print("Masukkan kolom (1-2): ");
-                        int kolom = sc.nextInt();
-                        sc.nextLine();
 
-                        if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                        int baris, kolom;
+                        while (true) {
+                            System.out.print("Masukkan baris (1-4): ");
+                            baris = sc.nextInt();
+                            System.out.print("Masukkan kolom (1-2): ");
+                            kolom = sc.nextInt();
+                            sc.nextLine();
+
+                            if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
                                 System.out.println(" Nomor baris/kolom tidak tersedia!");
                             } else if (penonton[baris - 1][kolom - 1] != null) {
                                 System.out.println(" Kursi sudah ditempati oleh " + penonton[baris - 1][kolom - 1]);
                             } else {
                                 penonton[baris - 1][kolom - 1] = nama;
+                                break;
                             }
-                            
+                        }
+
                         System.out.print("Input penonton lainnya? (y/n): ");
                         String next = sc.nextLine();
                         if (next.equalsIgnoreCase("n")) break;
@@ -44,7 +49,11 @@ public class BioskopWithScanner04 {
                     System.out.println("\n=== Daftar Penonton ===");
                     for (int i = 0; i < penonton.length; i++) {
                         for (int j = 0; j < penonton[i].length; j++) {
-                            System.out.print(penonton[i][j] + "\t");
+                            if (penonton[i][j] == null) {
+                                System.out.print("***\t");
+                            } else {
+                                System.out.print(penonton[i][j] + "\t");
+                            }
                         }
                         System.out.println();
                     }
